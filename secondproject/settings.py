5020769dfeef091ccc5f3af320132f7c89a6d917
- <->
2025-08-05 01:40:59 +0500

    FIX: Corrected INSTALLED_APPS for django_bootstrap5

--- secondproject/settings.py
+++ secondproject/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'secondapp',
-    'django-bootstrap5',
+    'django_bootstrap5',
     'paypal.standard.ipn',
 ]
 
